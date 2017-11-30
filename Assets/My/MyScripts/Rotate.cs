b0f00e97f5255d1c0205c7d5cf0ea038f47d6d9d
Kudo <ku.kn8821da@gmail.com>
2017-11-30 19:22:07 +0900

    rotate

---
+++ Assets/My/MyScripts/Rotate.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour
+{
+
+    void Start()
+    {
+        StartCoroutine("MoveSphere");
+    }
+
+    void Update()
+    {
+       // transform.Rotate(new Vector3(0, 0, 5));
+    }
+
+    IEnumerator MoveSphere()
+    {
+        for (int f = 360; f >= 0; f--)
+        {
+            gameObject.transform.Rotate(0,f,0); 
+            yield return new WaitForSeconds(1.2f);
+            print(Time.time);
+        }
+    }
+
+}
