406da5d8b1d481ad5ca67c0c153dca5ee8106a03
Kudo <ku.kn8821da@gmail.com>
2017-11-30 16:50:45 +0900

    canon

---
+++ Assets/My/MyScripts/Shot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shot : MonoBehaviour {
+
+    public GameObject bullet;
+    public GameObject body;
+    public Transform muzzle;
+    public float speed = 1000;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player")) {
+            GameObject bullets = GameObject.Instantiate(bullet) as GameObject;
+            Vector3 force;
+            force = this.gameObject.transform.forward * speed;
+            bullets.GetComponent<Rigidbody>().AddForce(force);
+            bullet.transform.position = muzzle.position;
+            Destroy(body,0.05f);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
