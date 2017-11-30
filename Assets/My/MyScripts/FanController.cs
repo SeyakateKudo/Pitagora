7fcca1440319012b24571602b1440faeead969a0
Kudo <ku.kn8821da@gmail.com>
2017-11-30 18:11:03 +0900

    fan...

---
+++ Assets/My/MyScripts/FanController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanController : MonoBehaviour
+{
+
+    public GameObject wind;
+    public Transform fanmuzzle;
+    public float fanspeed = 1000;
+
+    IEnumerator Wind()
+    {
+            GameObject winds = GameObject.Instantiate(wind) as GameObject;
+            Vector3 force;
+            force = this.gameObject.transform.forward * fanspeed;
+            winds.GetComponent<Rigidbody>().AddForce(force);
+            wind.transform.position = fanmuzzle.position;
+            Destroy(winds, 2f);
+        yield return new WaitForSeconds(1f);
+        print(Time.time);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        //StartCoroutine("Wind");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        StartCoroutine("Wind");
+    }
+}
