390ea0361c64356bf80cd84db1d83426870fc9ca
Kudo <ku.kn8821da@gmail.com>
2017-12-08 16:00:11 +0900

    reload

---
+++ Assets/My/MyScripts/ReloadManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClick()
+    {
+
+
+        // 現在読み込んでいるシーンの名前を取得
+        string currentScene = SceneManager.GetActiveScene().name;
+        // 取得したシーン名で再読込み
+        SceneManager.LoadScene(currentScene);
+    }
+
+}
