29500e476d2d8f50e41257dc2f4bef8add394d86
Kudo <ku.kn8821da@gmail.com>
2018-02-28 16:55:56 +0900

    no message

---
+++ Assets/My/MyScripts/BarColor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarColor : MonoBehaviour {
+
+    public GameObject bar;
+    private bool flag = false;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        ColorChange();
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        flag = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        flag = false;
+    }
+
+    private void ColorChange()
+    {
+        if (flag == true)
+        {
+            bar.GetComponent<Renderer>().material.color = Color.green;
+        }
+        else
+        {
+            bar.GetComponent<Renderer>().material.color = Color.gray;
+        }
+
+
+    }
+}
+
---
+++ Assets/My/MyScripts/BarColorKoiru.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarColorKoiru : MonoBehaviour {
+
+    public GameObject bar;
+    public GameObject bar1;
+
+    bool flag = false;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ColorChange();
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        flag = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        flag = false;
+    }
+
+    void ColorChange()
+    {
+        if (flag == true)
+        {
+            bar.GetComponent<Renderer>().material.color = Color.green;
+            bar1.GetComponent<Renderer>().material.color = Color.green;
+        }
+        else
+        {
+            bar.GetComponent<Renderer>().material.color = Color.gray;
+            bar1.GetComponent<Renderer>().material.color = Color.gray;
+        }
+
+
+    }
+}
